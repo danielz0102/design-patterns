0edaf85f598c7fd785cd2155586b5fcd69008ee6
danielz0102 <danyzglez@hotmail.com>
2025-07-14 15:49:49 -0600

    Add chain of responsability pattern

---
+++ src/behavioral/chain-of-responsability/middlewares/BaseMiddleware.ts
@@ -0,0 +1,17 @@
+import { Middleware, Request } from "../types";
+
+export abstract class BaseMiddleware implements Middleware {
+  private nextMiddleware: Middleware | null = null;
+
+  handle(req: Request): void {
+    if (this.nextMiddleware) {
+      this.nextMiddleware.handle(req);
+    }
+  }
+
+  setNext(middleware: Middleware): Middleware {
+    this.nextMiddleware = middleware;
+
+    return this.nextMiddleware;
+  }
+}
---
+++ src/behavioral/chain-of-responsability/middlewares/Log.ts
@@ -0,0 +1,12 @@
+import { Request } from "../types";
+import { BaseMiddleware } from "./BaseMiddleware";
+
+export class Log extends BaseMiddleware {
+  handle(req: Request): void {
+    if (req.user) {
+      console.log(`User ${req.user.id} accessed ${req.url}`);
+    }
+
+    super.handle(req);
+  }
+}
