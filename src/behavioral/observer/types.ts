39928cf2eda99cbaf42de9ea6b1b275615b8ce88
danielz0102 <danyzglez@hotmail.com>
2025-07-23 23:49:25 -0600

    Add observer pattern

---
+++ src/behavioral/observer/types.ts
@@ -0,0 +1,20 @@
+export interface Observer {
+  update(event: EventType): void;
+}
+
+export interface Publisher {
+  subscribe(observer: Observer): void;
+  unsubscribe(observer: Observer): void;
+  notify(event: EventType): void;
+}
+
+export enum EventType {
+  DOCUMENT_CHANGED = "DOCUMENT_CHANGED",
+  DOCUMENT_RESET = "DOCUMENT_RESET",
+  DOCUMENT_PRINTED = "DOCUMENT_PRINTED",
+}
+
+export type HistoryEntry = {
+  event: EventType;
+  timestamp: Date;
+};
